e64a81f55cf3a947f23933ec625bcddda2d511a0
Cameron Conn <cam@camconn.cc>
2025-04-26 18:36:14 -0500

    Add file type metadata query to GGUF files

--- src/ggml.zig
+++ src/ggml.zig
@@ -224,6 +224,34 @@ pub const GGUFHeader = struct {
     }
 };
 
+/// GGUF File Type. Located in the `general.file_type` metadata value.
+pub const FileType = enum(u32) {
+    // zig fmt: off
+    ALL_F32              = 0,
+    MOSTLY_F16           = 1,
+    MOSTLY_Q4_0          = 2,
+    MOSTLY_Q4_1          = 3,
+    MOSTLY_Q4_1_SOME_F16 = 4,
+    MOSTLY_Q4_2          = 5, // support removed
+    MOSTLY_Q4_3          = 6, // support removed
+    MOSTLY_Q8_0          = 7,
+    MOSTLY_Q5_0          = 8,
+    MOSTLY_Q5_1          = 9,
+    MOSTLY_Q2_K          = 10,
+    MOSTLY_Q3_K_S        = 11,
+    MOSTLY_Q3_K_M        = 12,
+    MOSTLY_Q3_K_L        = 13,
+    MOSTLY_Q4_K_S        = 14,
+    MOSTLY_Q4_K_M        = 15,
+    MOSTLY_Q5_K_S        = 16,
+    MOSTLY_Q5_K_M        = 17,
+    MOSTLY_Q6_K          = 18,
+    /// Sentinel value used to check range, not actually in `ggml.h`
+    NUM_FILETYPE         = 19,
+    // zig fmt: on
+};
+const max_file_type = @intFromEnum(FileType.NUM_FILETYPE);
+
 // gguf_tensor_info_t
 pub const TensorInfo = struct {
     const Self = @This();
@@ -309,6 +337,7 @@ pub const alignment_key = "general.alignment";
 pub const quant_version_key = "general.quantization_version";
 pub const arch_key = "general.architecture";
 pub const name_key = "general.name";
+pub const file_type_key = "general.file_type";
 
 // gguf_file_t
 pub const GGUFFile = struct {
@@ -437,6 +466,21 @@ pub const GGUFFile = struct {
             }
         else
             "unset";
+
+        if (getMetadataValue(file_type_key, metadata)) |ft| {
+            switch (ft) {
+                .uint32 => {
+                    const val = ft.uint32;
+                    if (val >= max_file_type) {
+                        std.debug.print("ggml: Invalid file type: read {d}, but max is {d}\n", .{ val, max_file_type });
+                        return Error.Format;
+                    }
+                },
+                else => {
+                    std.debug.print("ggml: \"{s}\" must be a uint32\n", .{file_type_key});
+                },
+            }
+        }
 
         std.debug.print("Model architecture: {s}\n", .{arch});
         std.debug.print("Model name: {s}\n", .{name});
